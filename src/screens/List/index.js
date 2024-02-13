84037095fc63ca3ecf73bef6cc8fbdc7010f275c
MaximPoz <executioner2126@gmail.com>
2024-02-13 21:36:14 +0900

    List of premises

--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import style from './style.module.css'
 
-export const List = () =>{
+const API_PRODUCTS = 'https://fakestoreapi.com/products'
+
+
+export const List = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            try {
+                let response = await fetch(API_PRODUCTS)
+                let data = await response.json();
+                console.log(`Данные получены: ${items.length} объектов`);
+                setItems(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchItems();
+    }, []);
+
+
     return (
-        <div>
-            <h1>List of premises</h1>
+        <div> {/* Применяем стиль к родительскому контейнеру */}
+            <h1 className={style.head}>List of premises</h1>
+            <div className={style.container}>
+            {items.map((item) => (
+                <div className={style.item} key={item.id}> {/* Применяем стиль к каждому элементу */}
+                    <img className={style.imageClass} src={item.image} alt="" />
+                    <p className={style.textClass}>{item.title}</p>
+                    <p className={style.textClass}>Цена за номер: ${item.price}</p>
+                </div>
+            ))}
+            </div>
+            
         </div>
-    )
+    );
 }
